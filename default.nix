ecf2cc9966af6e303053fbd895f0617ee2ee42a1
Vilem Liepelt <17603372+buggymcbugfix@users.noreply.github.com>
2025-03-20 14:09:59 +0100

    Nix stuff

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
 let
   pinnedNixpkgs = import (builtins.fetchTarball {
     name = "pinned-nixpkgs-for-urweb-school";
-    url = https://github.com/NixOS/nixpkgs/archive/5a8bfc98a23669f71596d079df20730ccdfdf04b.tar.gz;
+    url = https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "15qbfjjw5ak1bpiq36s0y9iq3j45azmb8nz06fpx4dgkg32i8fm5";
+    sha256 = "10wn0l08j9lgqcw8177nh2ljrnxdrpri7bp0g7nvrsn9rkawvlbf";
   }) {};
 in
 {pkgs ? pinnedNixpkgs}: pkgs.callPackage ./derivation.nix {}
