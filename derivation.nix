d24d8c2e981884097d8e9dad0bc63fac17767216
Vilem Liepelt <17603372+buggymcbugfix@users.noreply.github.com>
2025-03-20 14:01:48 +0100

    Fix nix

--- derivation.nix
+++ derivation.nix
@@ -1,56 +1,70 @@
-{ stdenv, lib, fetchFromGitHub, file, openssl, mlton
-, mysql, postgresql, sqlite, gcc
-, automake, autoconf, libtool, icu, nix-gitignore
+{ lib, stdenv, fetchFromGitHub, file, openssl, mlton
+, mariadb-connector-c, postgresql, sqlite, gcc, icu, autoconf, automake, libtool
 }:
 
 stdenv.mkDerivation rec {
-  name = "urweb-${version}";
-  version = "2018-06-22";
-  
-  # src = fetchurl {
-  #   url = "http://www.impredicative.com/ur/${name}.tgz";
-  #   sha256 = "17qh9mcmlhbv6r52yij8l9ik7j7x6x7c09lf6pznnbdh4sf8p5wb";
-  # };
+  pname = "urweb";
+  version = "20200209";
 
-  # src = fetchFromGitHub {
-  #   owner = "FrigoEU";
-  #   repo = "urweb";
-  #   rev = "e52ce9f542f64750941cfd84efdb6d993ee20ff0";
-  #   sha256 = "19ba5n7g1dxy7q9949aakqplchsyzwrrnxv8v604vx5sg7fdfn3b";
-  # };
-  src = ./.;
+  src = fetchFromGitHub {
+    owner = "urweb";
+    repo = "urweb";
+    rev = "55a881f";
+    hash = "sha256-V39MLf8Y+o5PLaQXNkG0tDcMsxL/AiQmcbE40swUxaQ=";
+  };
 
-  buildInputs = [ openssl mlton mysql.connector-c postgresql sqlite automake autoconf libtool icu.dev openssl.dev];
+  buildInputs = [ openssl mlton mariadb-connector-c postgresql sqlite icu autoconf automake libtool];
 
-  # prePatch = ''
-  #   sed -e 's@/usr/bin/file@${file}/bin/file@g' -i configure
-  # '';
+  prePatch = ''
+    sed -e 's@/usr/bin/file@${file}/bin/file@g' -i configure.ac
+  '';
 
-  configureFlags = "--with-openssl=${openssl.dev}";
+  # patches = [
+	# 	patches/postgres_disable_integrity_check.patch
+	# 	patches/mysql-disable-integrity-check.uw.patch
+	# 	patches/mysql-distinct-from.uw.patch
+	# 	patches/mysql-my_bool.uw.patch
+	# 	patches/urweb_int_field_nm.patch
+	# 	patches/void.uw.patch
+  # ];
+
+  configureFlags = [ "--with-openssl=${openssl.dev}" ];
 
   preConfigure = ''
-    ./autogen.sh
-    export PGHEADER="${postgresql}/include/libpq-fe.h";
-    export MSHEADER="${mysql.connector-c}/include/mysql/mysql.h";
+    export MSHEADER="${mariadb-connector-c.dev}/include/mysql/mysql.h";
     export SQHEADER="${sqlite.dev}/include/sqlite3.h";
+    export PGHEADER="${postgresql.dev}/include/libpq-fe.h";
+    export ICU_INCLUDES="-I${icu.dev}/include";
+
     export CC="${gcc}/bin/gcc";
     export CCARGS="-I$out/include \
-                   -I${icu.dev}/include \
-                   -L${openssl.out}/lib \
-                   -L${mysql.connector-c}/lib \
+                   -L${lib.getLib openssl}/lib \
+                   -L${mariadb-connector-c}/lib/mysql \
                    -L${postgresql.lib}/lib \
-                   -L${sqlite.out}/lib \
-                   -L${icu.out}/lib";
-  '';
+                   -L${sqlite.out}/lib";
+    ./autogen.sh
+  '';                   #  -fsanitize=address";
+
+  env.NIX_CFLAGS_COMPILE = toString [
+    # Needed with GCC 12
+    "-Wno-error=use-after-free"
+  ];
 
   # Be sure to keep the statically linked libraries
   dontDisableStatic = true;
 
+  # buildPhase = "make";
+
+  # installPhase = "
+  #   cp ${application}.exe $out/${application}.exe
+  # ";
+
   meta = {
     description = "Advanced purely-functional web programming language";
+    mainProgram = "urweb";
     homepage    = "http://www.impredicative.com/ur/";
-    license     = stdenv.lib.licenses.bsd3;
-    platforms   = stdenv.lib.platforms.linux ++ stdenv.lib.platforms.darwin;
-    maintainers = [ stdenv.lib.maintainers.thoughtpolice stdenv.lib.maintainers.sheganinans ];
+    license     = lib.licenses.bsd3;
+    platforms   = lib.platforms.linux ++ lib.platforms.darwin;
+    maintainers = [ lib.maintainers.thoughtpolice lib.maintainers.sheganinans ];
   };
 }
