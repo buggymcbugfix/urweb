06815de24bdf52bd50a196629728449e87b841c2
Vilem Liepelt <17603372+buggymcbugfix@users.noreply.github.com>
2026-08-29 18:48:14 -0400

    default.nix: nixpkgs-25.05 ~> nixpkgs-26.05

--- default.nix
+++ default.nix
@@ -1,9 +1,8 @@
 let
   pinnedNixpkgs = import (builtins.fetchTarball {
-    name = "pinned-nixpkgs-for-urweb-school";
-    url = https://github.com/NixOS/nixpkgs/archive/25.05.tar.gz;
+    url = https://github.com/NixOS/nixpkgs/archive/26.05.tar.gz;
     # Hash obtained using `nix-prefetch-url --unpack <url>`
-    sha256 = "1915r28xc4znrh2vf4rrjnxldw2imysz819gzhk9qlrkqanmfsxd";
+    sha256 = "0am8xx09fx5yf2p0wb001v0jx1g5hrfb76h4r37xph378jgk7pcr";
   }) {};
 in
 {pkgs ? pinnedNixpkgs}: pkgs.callPackage ./derivation.nix {}
