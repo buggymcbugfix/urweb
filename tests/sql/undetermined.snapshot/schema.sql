c9937ee1de45ef4cf244d1d5b629db719084e0ad
Vilem Liepelt <17603372+buggymcbugfix@users.noreply.github.com>
2026-09-11 19:15:52 +0200

    reject stray files in snapshot dir

--- tests/sql/undetermined.snapshot/schema.sql
+++
@@ -1,9 +0,0 @@
-PRAGMA foreign_keys = ON;
-PRAGMA journal_mode = WAL;
-
-CREATE TABLE uw_Undetermined_t (
-    uw_Id integer NOT NULL,
-    uw_Name text NOT NULL,
-    CONSTRAINT uw_Undetermined_t_pkey PRIMARY KEY (uw_Id)
-);
-
